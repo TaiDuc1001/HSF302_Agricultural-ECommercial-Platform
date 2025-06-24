f845d5ff8520b661b260d4432a79f96d8cd8aafa
Tuan Anh <tuananhvn0203@gmail.com>
2025-06-25 01:01:43 +0700

    convert to Thymeleaf in components

--- frontend/src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
-  <footer className="bg-green-800 text-white text-center py-4 w-full z-50">
-    <div>© {new Date().getFullYear()} AgriMarket. All rights reserved.</div>
-  </footer>
-);
-
-export default Footer;
