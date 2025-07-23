8475cd62702e9a31ad0994f71ba05a3345d80851
gheiucuavanh <phnanhky@gmail.com>
2025-07-23 18:50:45 +0700

    fix category

--- src/main/java/main/service/category/CategoryServiceImpl.java
+++ src/main/java/main/service/category/CategoryServiceImpl.java
@@ -1,34 +1,23 @@
 package main.service.category;
 
 
+import main.dto.CategoryDTO;
 import main.pojo.Category;
 import main.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
-    CategoryRepository categoryRepository;
+    private CategoryRepository categoryRepository;
     @Override
     public List<Category> findAll() {
         return categoryRepository.findAll();
-
-import lombok.RequiredArgsConstructor;
-import main.dto.CategoryDTO;
-import main.repository.CategoryRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-
-@Service
-@RequiredArgsConstructor
-public class CategoryServiceImpl implements CategoryService{
-    private final CategoryRepository categoryRepository;
+    }
     @Override
     public List<CategoryDTO> getAllCategories() {
         return categoryRepository.findAll()
